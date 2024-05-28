817057f196fd4105dcbb09a6b7c32c32eb1a7baa
Eliana Dheys B. Ojeda <164065005+liandh@users.noreply.github.com>
2024-05-28 20:24:22 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/About/Hero/Hero";
-import Categories from "./components/Categories/Categories";
+import Categories from "./components/Navbar/Categories/Categories";
 import About from "./components/About/About";
-import Blog  from "./components/Blog/Blog";
+import Blog from "./components/Blog/Blog";
 import Addpage from "./components/Addpage";
 import ContactUs from "./components/ContactUs/ContactUs";
+import BookListModal from "./components/BookListModal";
 
 const App = () => {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedCategory(null);
+  };
+
   return (
     <>
       <div>
         <Navbar />
         <Hero />
-        <Categories />
+        <Categories onCategoryClick={handleCategoryClick} />
         <About />
         <Blog />
         <Addpage />
         <ContactUs />
+        {selectedCategory && (
+          <BookListModal
+            category={selectedCategory}
+            onClose={handleCloseModal}
+          />
+        )}
       </div>
     </>
   );
